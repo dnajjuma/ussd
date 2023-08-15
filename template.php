eb5e50859c0f8af933835d7372fadf492cd2d62f
dnajjuma <diananajjuma92@gmail.com>
2023-08-15 19:56:40 +0300

    make it better

--- template.php
+++ template.php
@@ -8,13 +8,13 @@ $text        = $_POST["text"];
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
     $response  = "CON What would you want to check \n";
-    $response .= "1. Payments \n";
+    $response .= "1. My Account \n";
     $response .= "2. My phone number";
 
 } else if ($text == "1") {
     // Business logic for first level response
-    $response = "CON Enter card no \n";
-    $response .= "1. My fee \n";
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
 
 } else if ($text == "2") {
     // Business logic for first level response
@@ -23,10 +23,10 @@ if ($text == "") {
 
 } else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
-    $bill  = "2000";
+    $accountNumber  = "ACC1001";
 
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your maintenance fee is ".$bill;
+    $response = "END Your account number is ".$accountNumber;
 
 }
 
