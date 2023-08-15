f2bdc283cef521e2a64e79d1ebdc284e5b93dc21
dnajjuma <diananajjuma92@gmail.com>
2023-08-15 19:12:52 +0300

    make it better

--- ussd.php
+++ ussd.php
@@ -3,32 +3,35 @@
 $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
+$cardNumber = $_POST["cardnumber"];
 $text        = $_POST["text"];
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "BOREPAY What would you want to check \n";
-    $response .= "1. My Account \n";
-    $response .= "2. My phone number";
-
-} else if ($text == "1") {
-    // Business logic for first level response
-    $response = "BOREPAY Choose account information you want to view \n";
-    $response .= "1. Account number \n";
+    $response  = "Welcome to BOREPAY, What would you want to do? \n";
+    $response .= "1. Make Payment \n";
+    $response .= "2. See my Card No";
+    $response .= "3. Phone number";
 
 } else if ($text == "2") {
+    // Business logic for first level response
+    $response = "BOREPAY Choose account information you want to view \n";
+    $response .= "1. Card number \n";
+
+} else if ($text == "3") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $response = "END Your phone number is ".$phoneNumber;
 
-} else if($text == "1*1") { 
+} else if($text == "2*2") { 
     // This is a second level response where the user selected 1 in the first instance
-    $accountNumber  = "ACC1001";
+    $cardNumber  = "CUI1001";
 
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your account number is ".$accountNumber;
+    $response = "END Your card number is ".$cardNumber;
 
 }
+
 
 // Echo the response back to the API
 header('Content-type: text/plain');
